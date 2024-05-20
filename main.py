3e4977a24f8b5d42974d33537901b240063a1afe
Njehia98 <stacynjihia028@gmail.com>
2024-05-20 20:04:25 +0300

    added argument body

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def send_email_notification(subject, message, sender_email, sender_password, recipient_email, smtp_server, smtp_port):
+def send_email_notification(subject, message, body, sender_email, sender_password, recipient_email, smtp_server, smtp_port):
     try:
         # Create the email header
         msg = MIMEMultipart()
         msg['From'] = sender_email
         msg['To'] = recipient_email
         msg['Subject'] = subject
-
+        msg['Body'] = body
         # Attach the email body
         msg.attach(MIMEText(message, 'plain'))
 
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     # Email details
     subject = "Notification from Python Script"
     notification_message = "You have been hacked!!!"
+    body= "This is a body"
     sender_email = "s.njehia@chem-labs.com"  
     sender_password = "k7%45AenhS" 
     recipient_email = "c.njoroge@chem-labs.com"  
@@ -34,4 +35,4 @@ if __name__ == "__main__":
     smtp_port = 587  
 
     # Send the email notification
-    send_email_notification(subject, notification_message, sender_email, sender_password, recipient_email, smtp_server, smtp_port)
+    send_email_notification(subject, notification_message,body, sender_email, sender_password, recipient_email, smtp_server, smtp_port)
